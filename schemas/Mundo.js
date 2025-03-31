25a0126becc9822f7f96a3b8a6a41863c00b9798
Borjaeditions <borja_and_borjaeditions@live.com>
2025-03-31 14:42:39 -0600

    problema dashboard

--- schemas/Mundo.js
+++ schemas/Mundo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const mundoSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   puerto: { type: Number, required: true },
-  ram: { type: Number, required: true },
+  memoria: { type: Number, required: true },
   estado: { type: String, default: "apagado" },
   jugadores: [String],
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' }
